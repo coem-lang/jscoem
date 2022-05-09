6ef0fdb7e019b3c1b1dd583ec3afda43dc04a4dd
Katherine Yang <yang110@usc.edu>
2022-05-09 11:37:29 -0700

    chore: remove exports from build

--- dist/coem.js
+++ dist/coem.js
@@ -1081,4 +1081,4 @@ function parse(code) {
   return statements;
 }
 
-export { Environment, Interpreter, Parser, Tokenizer, formatCoemError, parse, run };
+// export { Environment, Interpreter, Parser, Tokenizer, formatCoemError, parse, run };
